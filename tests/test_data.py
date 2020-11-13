2e592baff52d576f2a4b3a2ea59f0eca3545f7a7
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2020-11-13 14:32:05 +0000

    update tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,9 @@
 import pytest
 from unittest import mock
 
-from ci_mapping.data.query_mag_composite import build_expr
-from ci_mapping.data.query_mag_composite import query_mag_api
-from ci_mapping.data.query_mag_composite import build_composite_expr
-from ci_mapping.data.query_mag_composite import build_composite_expr_date
+from ci_mapping.data.query_mag import build_expr
+from ci_mapping.data.query_mag import query_mag_api
+from ci_mapping.data.query_mag import build_composite_expr
 
 
 class TestBuildExpr:
@@ -21,34 +20,25 @@ class TestBuildExpr:
         ]
 
 
-def test_build_composite_queries_correctly():
-    assert (
-        build_composite_expr(["dog", "cat"], "F.FN", 2000)
-        == "expr=OR(And(Composite(F.FN='dog'), Y>=2000), And(Composite(F.FN='cat'), Y>=2000))"
-    )
-
-
-def test_build_composite_by_date_queries_correctly():
-    assert (
-        build_composite_expr_date(
-            ["dog", "cat"], "F.FN", 2000, (("01", "06"), ("01", "01"))
-        )
-        == "expr=OR(And(Composite(F.FN='dog'), D=['2000-01-01','2000-06-01']), And(Composite(F.FN='cat'), D=['2000-01-01','2000-06-01']))"
-    )
-
-
-@mock.patch("fnf.data.query_mag_composite.requests.post", autospec=True)
+@mock.patch("ci_mapping.data.query_mag.requests.post", autospec=True)
 def test_query_mag_api_sends_correct_request(mocked_requests):
     sub_key = 123
     fields = ["Id", "Ti"]
-    expr = "expr=OR(And(Composite(F.FN='dog'), Y>=2000), And(Composite(F.FN='cat'), Y>=2000))"
+    expr = "expr=OR(Id=1,Id=2)"
     query_mag_api(expr, fields, sub_key, query_count=10, offset=0)
     expected_call_args = mock.call(
         "https://api.labs.cognitive.microsoft.com/academic/v1.0/evaluate",
-        data=b"expr=OR(And(Composite(F.FN='dog'), Y>=2000), And(Composite(F.FN='cat'), Y>=2000))&count=10&offset=0&model=latest&attributes=Id,Ti",
+        data=b"expr=OR(Id=1,Id=2)&count=10&offset=0&attributes=Id,Ti",
         headers={
             "Ocp-Apim-Subscription-Key": 123,
             "Content-Type": "application/x-www-form-urlencoded",
         },
     )
     assert mocked_requests.call_args == expected_call_args
+
+
+def test_build_composite_queries_correctly():
+    assert (
+        build_composite_expr(["bar", "foo"], "F.FN", ("2019-01-01", "2019-02-22"))
+        == "expr=OR(And(Composite(F.FN='bar'), D=['2019-01-01', '2019-02-22']), And(Composite(F.FN='foo'), D=['2019-01-01', '2019-02-22']))"
+    )
--- tests/test_mag_orms.py
+++ tests/test_mag_orms.py
@@ -12,7 +12,7 @@ load_dotenv(find_dotenv())
 class TestMag(unittest.TestCase):
     """Check that the MAG ORM works as expected"""
 
-    engine = create_engine(os.getenv("test_postgresdb"))
+    engine = create_engine(os.getenv("postgres"))
     Session = sessionmaker(engine)
 
     def setUp(self):
